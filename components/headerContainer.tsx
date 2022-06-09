9110be87a385e90aec938b86f69b349ac07f33fb
Grace Wang <gracewang92@gmail.com>
2022-06-09 13:36:34 -0500

    move WalletProvider to _app.tsx

--- components/headerContainer.tsx
+++ components/headerContainer.tsx
@@ -1,36 +1,19 @@
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-ant-design';
+import { WalletMultiButton } from '@solana/wallet-adapter-ant-design';
 import '@solana/wallet-adapter-ant-design/styles.css';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { WalletProvider } from '@solana/wallet-adapter-react';
-import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { Layout } from 'antd';
 import { Header } from 'antd/lib/layout/layout';
-import { FunctionComponent, HTMLAttributes, useMemo } from 'react';
+import { FunctionComponent, HTMLAttributes } from 'react';
 
 const HeaderContainer: FunctionComponent<HTMLAttributes<HTMLDivElement>> = (props) => {
-  const network = WalletAdapterNetwork.Devnet;
-  const wallets = useMemo(
-    () => [
-      new SolflareWalletAdapter({ network }),
-      new SlopeWalletAdapter(),
-      new PhantomWalletAdapter(),
-      new GlowWalletAdapter(),
-    ],
-    [network]
-  );
 
   return (
-    <WalletProvider wallets={wallets} autoConnect>
-      <WalletModalProvider>
-        <Layout className="pearl-container" style={{ padding: "16px" }}>
-          <Header style={{ height: "54px", display: "flex", padding: "0 20px", marginBottom: "16px", justifyContent: "space-between", alignItems: "center", background: "white", borderRadius: "8px" }}>
-            <div style={{ fontSize: "20px" }}>Pearl</div>
-            <WalletMultiButton />
-          </Header>
-          {props.children}
-        </Layout>
-      </WalletModalProvider>
-    </WalletProvider>
+    <Layout className="pearl-container" style={{ padding: "16px" }}>
+      <Header style={{ height: "54px", display: "flex", padding: "0 20px", marginBottom: "16px", justifyContent: "space-between", alignItems: "center", background: "white", borderRadius: "8px" }}>
+        <div style={{ fontSize: "20px" }}>Pearl</div>
+        <WalletMultiButton />
+      </Header>
+      {props.children}
+    </Layout>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,35 @@
+import { WalletModalProvider } from '@solana/wallet-adapter-ant-design';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useMemo } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div>
-    <Head>
-      <title>Pearl: NFT Announcements</title>
-      <meta name="description" content="Schedule and send verified announcements to your holders." />
-      <link rel="icon" href="/pearl_100.jpg" />
-    </Head>
-  <Component {...pageProps} />
-  </div>
+  const network = WalletAdapterNetwork.Devnet;
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter({ network }),
+      new SlopeWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return <WalletProvider wallets={wallets} autoConnect>
+    <WalletModalProvider>
+      <Head>
+        <title>Pearl: NFT Announcements</title>
+        <meta name="description" content="Schedule and send verified announcements to your holders." />
+        <link rel="icon" href="/pearl_100.jpg" />
+      </Head>
+      <Component {...pageProps} />
+    </WalletModalProvider>
+  </WalletProvider>
 
 }
 
